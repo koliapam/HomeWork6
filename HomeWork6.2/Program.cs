4b8d06d38ac5b2711c478720ddb809e7e82ff25e
koliapam <pamkolya228@gmail.com>
2025-03-23 12:36:17 +0100

    Add project files.

---
+++ HomeWork6.2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+
+enum StoreType
+{
+    Продовольчий,
+    Господарський,
+    Одяг,
+    Взуття
+}
+
+
+class Store : IDisposable
+{
+   
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public StoreType Type { get; set; }
+    private bool disposed = false;
+
+   
+    public Store(string name, string address, StoreType type)
+    {
+        Name = name;
+        Address = address;
+        Type = type;
+        Console.WriteLine("Створено магазин: " + Name);
+    }
+
+    
+    public void DisplayInfo()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine($"Назва магазину: {Name}\nАдреса: {Address}\nТип: {Type}\n");
+    }
+
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                
+                Console.WriteLine("Звільняємо ресурси магазину: " + Name);
+            }
+
+            disposed = true;
+        }
+    }
+
+    // Деструктор
+    ~Store()
+    {
+        Dispose(false);
+        Console.WriteLine("Знищено магазин: " + Name);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        using (Store store1 = new Store("АТБ", "вул. Люстдорфська, 162", StoreType.Продовольчий))
+        {
+            store1.DisplayInfo();
+        } 
+
+        Console.WriteLine("Магазин видалено");
+    }
+}
---
+++ HomeWork6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Play : IDisposable
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public int Year { get; set; }
+    private bool disposed = false;
+
+    public Play(string title, string author, string genre, int year)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Year = year;
+        Console.WriteLine("Створено п'єсу: " + Title);
+    }
+
+    public void DisplayInfo()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine($"Назва: {Title}\nАвтор: {Author}\nЖанр: {Genre}\nРік: {Year}\n");
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Звільняємо ресурси п'єси: " + Title);
+            }
+
+            disposed = true;
+        }
+    }
+
+    ~Play()
+    {
+        Dispose(false);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        using (Play play1 = new Play("Річард III", "Вільям Шекспір", "Історічна Хроніка", 1594))
+        {
+            play1.DisplayInfo();
+        }
+
+        Console.WriteLine("П'єса видалена");
+    }
+}
